b3e685b8fac056695a614890f1c271738976f817
Sebastian Haas <sebastian@haas.tech>
2016-06-09 19:10:00 +0200

    Improved automigrate to migrate all models.

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -2,7 +2,8 @@ var path = require('path');
 var app = require(path.resolve(__dirname, '../server/server'));
 
 var ds = app.datasources.postgresql;
-ds.automigrate('Appointment', function(err) {
-  if (err) throw err;
-  ds.disconnect();
-});
+ds.automigrate()
+  .then(function(err) {
+    if (err) throw err;
+    ds.disconnect();
+  });
