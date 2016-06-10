a202fe861fcae192d69501da1dcf7842507f48c2
Sebastian Haas <sebastian@haas.tech>
2016-06-10 12:40:54 +0200

    Introduced bunyan as logging system. Added syslog stream for RFC 5424 conformity.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,45 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var bunyan = require('bunyan');
+var bunyanRequest = require('bunyan-request');
+var bunyanSyslog = require('bunyan-syslog');
 
 var app = module.exports = loopback();
+
+// Set up logging
+var logger = bunyan.createLogger({
+  name: 'medical-appointment-scheduling-server',
+  streams: [
+  {
+    level: 'debug',
+    type: 'raw',
+    stream: bunyanSyslog.createBunyanStream({
+      type: 'udp',
+      facility: bunyanSyslog.local0,
+      host: '127.0.0.1',
+      port: 11111
+    })
+  },
+  {
+    level: 'debug',
+    stream: process.stdout
+  }]
+});
+var requestLogger = bunyanRequest({
+  logger: logger,
+  headerName: 'x-request-id'
+});
+app.use(requestLogger);
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    logger.info('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      logger.info('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
