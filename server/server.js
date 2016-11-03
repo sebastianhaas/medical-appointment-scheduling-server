d047aa87cebf48156cb846473bb09ed0a59189c9
Sebastian Haas <sebastian@haas.tech>
2016-11-03 02:22:39 +0100

    Added CantyCTI telephony service.
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

--- server/server.js
+++ server/server.js
@@ -51,6 +51,48 @@ boot(app, __dirname, function(err) {
 
   // start the server if `$ node server.js`
   if (require.main === module) {
-    app.start();
+    // Start the server
+    var expressApp = app.start();
+
+    // Start CantyCTI
+    cantyCti(expressApp);
   }
 });
+
+function cantyCti(expressApp) {
+
+  /**
+   * Keys for Socket.io events.
+   */
+  const EVENT_KEY_INCOMING_CALL = 'incoming call';
+  const EVENT_KEY_CALL_REQUEST = 'call request';
+
+  const io = require('socket.io')(expressApp);
+  io.on('connection', ctiOnConnection);
+
+  /**
+   * Use this function on socket.io connection event.
+   */
+  function ctiOnConnection(socket) {
+    logger.info('Client connected.');
+    logger.info('A total of %d clients are connected.', io.engine.clientsCount);
+
+    socket.on(EVENT_KEY_INCOMING_CALL, function(callInfo) {
+      // Broadcast incoming call event to all other clients
+      socket.broadcast.emit(EVENT_KEY_INCOMING_CALL, callInfo);
+      logger.info('Incoming call event from %s is %d. [%s]',
+        callInfo.phoneNumber, callInfo.callState, callInfo.id);
+    });
+
+    socket.on(EVENT_KEY_CALL_REQUEST, function(callRequest) {
+      // Broadcast incoming call event to all other clients
+      socket.broadcast.emit(EVENT_KEY_CALL_REQUEST, callRequest);
+      logger.info('Requested to call %s.', callRequest.phoneNumber);
+    });
+
+    socket.once('disconnect', function() {
+      logger.info('Client disconnected.');
+      logger.info('A total of %d clients are connected.', io.engine.clientsCount);
+    });
+  }
+}
